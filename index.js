c4d63107c70519963b7113485ec42139db1974d3
Seine <seinethizar.m@j-dea.com>
2024-01-24 21:31:13 +0700

    ngaseine

--- index.js
+++ index.js
@@ -6,40 +6,40 @@ function changeText(id, text) {
   document.getElementById(id).lastChild.data = text;
 }
 
-document.getElementById('newoption').addEventListener("mouseover", () => changeText('newoption', 'Ctrl+N'));
-document.getElementById("newoption").addEventListener("mouseout", () => changeText('newoption', 'New'));
-document.getElementById('openoption').addEventListener("mouseover", () => changeText('openoption', 'Ctrl+O'));
-document.getElementById("openoption").addEventListener("mouseout", () => changeText('openoption', 'Open'));
-document.getElementById('runoption').addEventListener("mouseover", () => changeText('runoption', 'Ctrl+R'));
-document.getElementById("runoption").addEventListener("mouseout", () => changeText('runoption', 'Run'));
-document.getElementById('saveoption').addEventListener("mouseover", () => changeText('saveoption', 'Ctrl+S'));
-document.getElementById("saveoption").addEventListener("mouseout", () => changeText('saveoption', 'Save'));
-document.getElementById('saveasoption').addEventListener("mouseover", () => changeText('saveasoption', 'Ctrl+Shift'));
-document.getElementById("saveasoption").addEventListener("mouseout", () => changeText('saveasoption', 'Save As...'));
-document.getElementById('printoption').addEventListener("mouseover", () => changeText('printoption', 'Ctrl+P'));
-document.getElementById("printoption").addEventListener("mouseout", () => changeText('printoption', 'Print'));
+document.getElementById('newoption').addEventListener("mouseover", () => changeText('newoption', ' Ctrl+N'));
+document.getElementById("newoption").addEventListener("mouseout", () => changeText('newoption', ' New'));
+document.getElementById('openoption').addEventListener("mouseover", () => changeText('openoption', ' Ctrl+O'));
+document.getElementById("openoption").addEventListener("mouseout", () => changeText('openoption', ' Open'));
+document.getElementById('runoption').addEventListener("mouseover", () => changeText('runoption', ' Ctrl+R'));
+document.getElementById("runoption").addEventListener("mouseout", () => changeText('runoption', ' Run'));
+document.getElementById('saveoption').addEventListener("mouseover", () => changeText('saveoption', ' Ctrl+S'));
+document.getElementById("saveoption").addEventListener("mouseout", () => changeText('saveoption', ' Save'));
+document.getElementById('saveasoption').addEventListener("mouseover", () => changeText('saveasoption', ' Ctrl+Shift'));
+document.getElementById("saveasoption").addEventListener("mouseout", () => changeText('saveasoption', ' Save As...'));
+document.getElementById('printoption').addEventListener("mouseover", () => changeText('printoption', ' Ctrl+P'));
+document.getElementById("printoption").addEventListener("mouseout", () => changeText('printoption', ' Print'));
 
-document.getElementById('undooption').addEventListener("mouseover", () => changeText('undooption', 'Ctrl+Z'));
-document.getElementById("undooption").addEventListener("mouseout", () => changeText('undooption', 'Undo'));
-document.getElementById('redooption').addEventListener("mouseover", () => changeText('redooption', 'Ctrl+Y'));
-document.getElementById("redooption").addEventListener("mouseout", () => changeText('redooption', 'Redo'));
-document.getElementById('cutoption').addEventListener("mouseover", () => changeText('cutoption', 'Ctrl+X'));
-document.getElementById("cutoption").addEventListener("mouseout", () => changeText('cutoption', 'Cut'));
-document.getElementById('copyoption').addEventListener("mouseover", () => changeText('copyoption', 'Ctrl+C'));
-document.getElementById("copyoption").addEventListener("mouseout", () => changeText('copyoption', 'Copy'));
-document.getElementById('pasteoption').addEventListener("mouseover", () => changeText('pasteoption', 'Ctrl+V'));
-document.getElementById("pasteoption").addEventListener("mouseout", () => changeText('pasteoption', 'Paste'));
-document.getElementById('selectalloption').addEventListener("mouseover", () => changeText('selectalloption', 'Ctrl+A'));
-document.getElementById("selectalloption").addEventListener("mouseout", () => changeText('selectalloption', 'Select All'));
+document.getElementById('undooption').addEventListener("mouseover", () => changeText('undooption', ' Ctrl+Z'));
+document.getElementById("undooption").addEventListener("mouseout", () => changeText('undooption', ' Undo'));
+document.getElementById('redooption').addEventListener("mouseover", () => changeText('redooption', ' Ctrl+Y'));
+document.getElementById("redooption").addEventListener("mouseout", () => changeText('redooption', ' Redo'));
+document.getElementById('cutoption').addEventListener("mouseover", () => changeText('cutoption', ' Ctrl+X'));
+document.getElementById("cutoption").addEventListener("mouseout", () => changeText('cutoption', ' Cut'));
+document.getElementById('copyoption').addEventListener("mouseover", () => changeText('copyoption', ' Ctrl+C'));
+document.getElementById("copyoption").addEventListener("mouseout", () => changeText('copyoption', ' Copy'));
+document.getElementById('pasteoption').addEventListener("mouseover", () => changeText('pasteoption', ' Ctrl+V'));
+document.getElementById("pasteoption").addEventListener("mouseout", () => changeText('pasteoption', ' Paste'));
+document.getElementById('selectalloption').addEventListener("mouseover", () => changeText('selectalloption', ' Ctrl+A'));
+document.getElementById("selectalloption").addEventListener("mouseout", () => changeText('selectalloption', ' Select All'));
 
-document.getElementById('mathoption').addEventListener("mouseover", () => changeText('mathoption', 'Ctrl+M'));
-document.getElementById("mathoption").addEventListener("mouseout", () => changeText('mathoption', 'Math'));
-document.getElementById('codeoption').addEventListener("mouseover", () => changeText('codeoption', 'Ctrl+T'));
-document.getElementById("codeoption").addEventListener("mouseout", () => changeText('codeoption', 'Code'));
-document.getElementById('fullscreenoption').addEventListener("mouseover", () => changeText('fullscreenoption', 'F11'));
-document.getElementById("fullscreenoption").addEventListener("mouseout", () => changeText('fullscreenoption', 'Full Screen'));
-document.getElementById('wysiwygoption').addEventListener("mouseover", () => changeText('wysiwygoption', 'Ctrl+W'));
-document.getElementById("wysiwygoption").addEventListener("mouseout", () => changeText('wysiwygoption', 'WYSIWYG'));
+document.getElementById('mathoption').addEventListener("mouseover", () => changeText('mathoption', ' Ctrl+M'));
+document.getElementById("mathoption").addEventListener("mouseout", () => changeText('mathoption', ' Math'));
+document.getElementById('codeoption').addEventListener("mouseover", () => changeText('codeoption', ' Ctrl+T'));
+document.getElementById("codeoption").addEventListener("mouseout", () => changeText('codeoption', ' Code'));
+document.getElementById('fullscreenoption').addEventListener("mouseover", () => changeText('fullscreenoption', ' F11'));
+document.getElementById("fullscreenoption").addEventListener("mouseout", () => changeText('fullscreenoption', ' Full Screen'));
+document.getElementById('wysiwygoption').addEventListener("mouseover", () => changeText('wysiwygoption', ' Ctrl+W'));
+document.getElementById("wysiwygoption").addEventListener("mouseout", () => changeText('wysiwygoption', ' WYSIWYG'));
 
 //This function is to uncheck any open submenus that were open when new submenu is clicked. Only one submenu can possibly stay open at a time.
 var checkboxes = ["check01", "check02", "check03"];
